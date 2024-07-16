454e6d8e862cfe7a579d6724e46743194a19e8ae
Irena Chen <irena@umich.edu>
2024-07-16 11:22:22 +0200

    Update joint_model_cluster_run.R
    add more comments/explanations for code and delete unnecessary content

--- R/joint_model_cluster_run.R
+++ R/joint_model_cluster_run.R
@@ -12,29 +12,34 @@ library(StanHeaders)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
+## set seed 
+seed = 607
 ##---------------------------------------
 # import the data 
 ##---------------------------------------
-## we have already excluded all of the people who were on hormone therapy i
-hormone_data <- data.frame(read.csv("/home/irena/gsra/swan_sims/data_analysis/visceral_fat/hormone_whr_10132022.csv"))
-bm_data <- read.csv("/home/irena/gsra/swan_sims/data_analysis/visceral_fat/whr_data_10132022.csv")
 
-seed = 607
+## predictor data (longitudinal)
+hormone_data <- data.frame(read.csv("hormone_whr_10132022.csv"))
+
+## outcome data (cross-sectional)
+bm_data <- read.csv("whr_data_10132022.csv")
 
 Q = 2 # no of hormones
-P = 2 # no of basis functions 
-I = length(unique(bm_data$new_id))
+P = 2 # no of basis functions for each marker 
+I = length(unique(bm_data$new_id)) ## number of individuals 
 
+## matrix for the predictor (markers)
 hormone_mat <- data.matrix(hormone_data[, c("e2_loess_resid","fsh_loess_resid")])
 
+## covariates in the outcome 
 cov_mat <- bm_data[,c("first_waist","black_ind", "chin_ind" ,"jpn_ind", "his_ind", "sports_cat2", "sports_cat3", "sports_cat4", "sports_cat5")]
 ##---------------------------------------:
 ## compile and run stan model: 
 ##---------------------------------------
 
-joint_model <- stan_model("/home/irena/gsra/swan_sims/data_analysis/visceral_fat/joint_model_mixture_scale_w_cor.stan")
+joint_model <- stan_model("joint_model_mixture_scale_w_cov.stan")
 
-sim_out <- sampling(joint_model, sample_file=paste0('/home/irena/gsra/swan_sims/data_analysis/visceral_fat/1013_waist_model_samples.csv'),
+sim_out <- sampling(joint_model, 
                          iter = 2000,
                          warmup=1000, #BURN IN
                          chains = 4,
@@ -47,12 +52,11 @@ sim_out <- sampling(joint_model, sample_file=paste0('/home/irena/gsra/swan_sims/
                                                          id=hormone_data$new_id,
                                                          f_time_fmp = cbind(1, hormone_data$time_fmp),
                                                          hormones =hormone_mat,
-                                                         n_cov = 9, 
- 							 K = 2,
-							 dirichlet_alpha=1,
+                                                         n_cov = 9,
+                                                         K = 2,
+                                                         dirichlet_alpha=1,
                                                          other_covariates = cov_mat,
                                                          bm_outcome = bm_data$waist_rate_change,
-                                                         simulate = 0
-                                             ),
-                         seed=seed
+                                                         simulate = 0),
+                    seed=seed
                         )
